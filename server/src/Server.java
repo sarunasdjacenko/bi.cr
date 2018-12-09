54242210a72e774b8dd41d4ec769a144caf62822
Sarunas Djacenko <fsarunas@protonmail.ch>
2018-12-09 07:57:08 +0000

    implement basic server

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,3 +1,6 @@
+import org.json.JSONWriter;
+import org.json.JSONArray;
+
 import java.io.DataOutputStream;
 import java.io.DataInputStream;
 import java.io.OutputStream;
@@ -6,22 +9,60 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Server implements Runnable {
     private int serverPort = 7777;
     private ServerSocket serverSocket;
-    private List<Socket> clientsInLobby;
-    private Debate debate;
+    private Map<String, ClientConnection> clientsInLobby;
+    private Map<String, Debate> debates;
 
     public Server() {
-        debate = new Debate("K-pop");
-        clientsInLobby = new ArrayList<Socket>();
+        clientsInLobby = new HashMap<String, ClientConnection>();
+        debates = new HashMap<String, Debate>();
+    }
+
+    public void addToLobby(String username, ClientConnection client) { clientsInLobby.put(username, client); System.out.println("User created: " + username); }
+
+    public void sendDebates(Socket clientSocket) {
+        StringBuffer JSONMessage = new StringBuffer();
+        new JSONWriter(JSONMessage)
+            .object()
+                .key("requestType").value("getDebates")
+                .key("debates").value(new JSONArray(debates.keySet()))
+            .endObject();
+
+        try {
+            DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+            dataOutputStream.writeUTF(JSONMessage.toString());
+        } catch(Exception e) { e.printStackTrace(); }
+    }
+
+    public void createDebate(String debateName) {
+        if (!debates.containsKey(debateName)) {
+            Debate debate = new Debate(debateName);
+            debates.put(debateName, debate);
+        }
+    }
+
+    public void joinDebate(String debateName, String username, Boolean isForArgument) {
+        ClientConnection client = clientsInLobby.get(username);
+        clientsInLobby.remove(username);
+        Debate debate = debates.get(debateName);
+        debate.addClient(client, username, isForArgument);
+    }
+
+    public void leaveDebate(Debate debate, String username) {
+        ClientConnection client = debate.getClient(username);
+        debate.removeClient(client, username);
+        clientsInLobby.put(username, client);
     }
 
     @Override
     public void run() {
         try {
-			serverSocket = new ServerSocket(serverPort);
+            serverSocket = new ServerSocket(serverPort);
 		} catch (Exception e) {
             System.out.println("Failed to create server.");
             System.exit(1);
@@ -30,19 +71,14 @@ public class Server implements Runnable {
 		System.out.println("--- SERVER CREATED ---");
 		while(true) {
 			try {
-				Socket clientSocket = serverSocket.accept();
-                clientsInLobby.add(clientSocket);
-                debate.addClient(clientSocket, true);
-
-                //System.out.println("\n --- NEW CLIENT HAS CONNECTED TO THE SERVER ---");
-				//System.out.println("\nCLIENT'S IP ADDRESS: " + clientSocket.getInetAddress().getHostAddress());
+                Socket clientSocket = serverSocket.accept();
+                
+                System.out.println("\n --- NEW CLIENT HAS CONNECTED TO THE SERVER ---");
+				System.out.println("\nCLIENT'S IP ADDRESS: " + clientSocket.getInetAddress().getHostAddress());
 
                 ClientConnection client = new ClientConnection(clientSocket, this);
-                //
-                client.setDebate(debate);
-                //
+
                 new Thread(client).start();
-                //Runnable client = new ClientConnection(clientSocket)
                 
 			} catch (Exception e) {}
 		}
@@ -51,6 +87,5 @@ public class Server implements Runnable {
     public static void main(String[] args) {
         Server server = new Server();
         new Thread(server).start();
-        //Runnable server = new Server();
     }
 }
